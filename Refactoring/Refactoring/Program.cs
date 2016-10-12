aff1602284c78047c91a909c3af051c2c8457b20
Mark Dewey <mark.t.dewey@gmail.com>
2016-10-12 10:38:01 -0400

    this is getting out of hand

---
+++ Refactoring/Refactoring/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refactoring
+{
+    class Program
+    {
+
+        //final goal is to have a location heiracrhy
+        
+            // each location has a name, latitude and longitude
+       
+
+        static void Main(string[] args)
+        {
+            var loc1Name = "TIY";
+            var loc1Address = "123 over here";
+            var loc1Lat = 25;
+            var loc1Long = 82;
+
+            var loc2Name = "TIY";
+            var loc2Address = "123 over here";
+            var loc2Lat = 25;
+            var loc2Long = 82;
+
+            var loc3Name = "TIY";
+            var loc3Address = "123 over here";
+            var loc3Lat = 25;
+            var loc3Long = 82;
+
+            Console.ReadLine();
+        }
+    }
+}
